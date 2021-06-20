ccb23f9563a1f3bf286cbd0e08a6fa6198b04f31
leomod272 <leomod272@gmail.com>
2021-06-20 13:49:15 +0200

    try resolve cors

--- dea-ws/index.js
+++ dea-ws/index.js
@@ -24,7 +24,24 @@ const routerRecords = require('./route/records.js');
 const routerRecordsInterval = require('./route/records-interval.js');
 const routerUser = require('./route/user.js');
 
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.get('/', (req, res) => {
     res.send("Ciao questo é il Web Service di DEA project")
